375de707051186b82de0ff9e0d0988e831f2a0b5
ShaunK79 <wism79@gmail.com>
2025-04-14 14:22:14 +0100

    completed javascript, tested

--- public/script.js
+++ public/script.js
@@ -8,9 +8,30 @@ document.getElementById('highlightBtn').addEventListener('click', () => { const 
         light.style.backgroundColor = '#62ED00';
     });
 });
-
+// add and create
 document.getElementById('addComponentBtn').addEventListener('click', () => { const list = document.getElementById('componentList');
     const item = document.createElement('li');
     item.innerText = 'New Electrical Component';
     list.appendChild(item);
 });
+// insert element
+document.getElementById('insertFuseBtn').addEventListener('click', () => {
+    const fuseBoard = document.getElementById('fuseBoard');
+    const fuse = document.createElement('li');
+    fuse.innerText = 'Fuse'
+    fuseBoard.insertBefore(fuse, fuseBoard.firstElementChild);
+});
+// replace element
+document.getElementById('replaceLampBtn').addEventListener('click', () => {
+    const oldLamp = document.getElementById('lamp');
+    const newLamp = document.createElement('p');
+    newLamp.innerText = 'New LED Lamp Installed';
+    newLamp.id = 'lamp';
+    oldLamp.parentNode.replaceChild(newLamp, oldLamp);
+});
+// remove element
+document.getElementById('removeWireBtn').addEventListener('click', () => {
+    const wire = document.getElementById('wire');
+    wire.remove();
+});
+
