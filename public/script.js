17d1c14c80db75aff480567a5fe15c7353c46d4e
ShaunK79 <wism79@gmail.com>
2025-04-14 13:48:41 +0100

    javascript - functions declared, single element, multiple element, creat&add element

--- public/script.js
+++ public/script.js
@@ -1,10 +1,16 @@
-// switch
+// single - switch
 document.getElementById('switchBtn').addEventListener('click', () => { const circuit = document.getElementById('circuit');
 circuit.innerText = ' The Circuit is ON';
 });
-
-document.getElementById('highlightBtn').addEventListener('click', () => { const lights = document.querySelectorAll('#lightBoard');
+// multiple - highlight
+document.getElementById('highlightBtn').addEventListener('click', () => { const lights = document.querySelectorAll('#lightBoard li');
     lights.forEach(light => {
-        light.computedStyleMap.backgroundColor = 'green';
+        light.style.backgroundColor = '#62ED00';
     });
+});
+
+document.getElementById('addComponentBtn').addEventListener('click', () => { const list = document.getElementById('componentList');
+    const item = document.createElement('li');
+    item.innerText = 'New Electrical Component';
+    list.appendChild(item);
 });
